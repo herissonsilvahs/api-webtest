0f90fc28084464413b3ef6de41dffba011b4e0e7
herissonsilvahs <herissonsilvahs@gmail.com>
2018-05-15 13:02:53 -0300

    setup database connection

--- configs/configs.php
+++ configs/configs.php
@@ -21,8 +21,8 @@ return [
             'username' => $db['user'],
             'password' => $db['pass'],
             'port' => $db['port'],
-            'charset'   => 'utf8mb4',
-            'collation' => 'utf8mb4_general_ci',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_general_ci',
             'prefix'    => '',
         ]
     ],
